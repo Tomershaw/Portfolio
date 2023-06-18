e76717bbab40ba3a1137690b9c1e6072a3de526f
tomer shaw <shaw.tomer@gmail.com>
2023-06-18 12:47:40 +0300

    I changed the size of the computer on the cell phone and lowered the ball under the computer

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -32,7 +32,7 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={loadedObject}
-        scale={isMobile ? 0.7 : 0.75}
+        scale={isMobile ? 0.5 : 0.75}
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
