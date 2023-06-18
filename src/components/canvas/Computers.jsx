84ecb975d0151ca36eafe15f800a8e9386980fb7
tomer shaw <shaw.tomer@gmail.com>
2023-06-18 13:42:41 +0300

    change the computer loction

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -33,7 +33,7 @@ const Computers = ({ isMobile }) => {
       <primitive
         object={loadedObject}
         scale={isMobile ? 0.5 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        position={isMobile ? [0, -3, -2.2] : [0, -2.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -66,7 +66,7 @@ const ComputersCanvas = () => {
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
-      <Suspense fallback={<CanvasLoader />}>
+      <Suspense fallback={<CanvasLoader/>}>
         <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
